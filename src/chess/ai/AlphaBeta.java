38bd17307feb326ed74b72c3bcec78987667de22
nguyenmv2 <nguyenmv2@hendrix.edu>
2015-09-17 14:58:17 -0500

    Change the parameters
    Only working with alpha now

--- src/chess/ai/AlphaBeta.java
+++ src/chess/ai/AlphaBeta.java
@@ -8,28 +8,24 @@ public class AlphaBeta extends Searcher {
 	public MoveScore findBestMove(Chessboard board, BoardEval eval, int depth) {
 		setup(board, eval, depth);
 		MoveScore result = evalMoves(board, eval, depth,-eval.maxValue(),eval.maxValue());
-		System.out.println("Result "+result.getScore() + result.getMove());
 		tearDown();
 		return result;
 	}
-	
+
 	MoveScore evalMoves(Chessboard board, BoardEval eval, int depth,int alpha, int beta) {
 		MoveScore best = null;
 		for (Move m: board.getLegalMoves()) {
 			Chessboard next = generate(board, m);
 			MoveScore result = new MoveScore(-evalBoard(next, eval, depth - 1, beta, alpha), m);
-			//if alpha < result -> alpha = result
-			//if beta > result -> beta = result
-			if (alpha<result.getScore()){
-				alpha=result.getScore();
-			}else if(beta > result.getScore()) {
-				beta = result.getScore();
+			if(alpha < result.getScore()) {
+				alpha = result.getScore();
+				result = new MoveScore(alpha,m);
 			}
 			if(alpha >= beta){
-				best = new MoveScore(alpha, m);
+				result = new MoveScore(alpha, m);
 				break;
-
 			}
+			if (best == null || best.getScore() < result.getScore()) best = result;
 		}
 
 		return best;
@@ -40,7 +36,7 @@ public class AlphaBeta extends Searcher {
 		if (depth == 0) {
 			return evaluate(board, eval);
 		} else {
-			return evalMoves(board, eval, depth,alpha,beta).getScore();
+			return evalMoves(board, eval, depth,beta,alpha).getScore();
 		}
 	}
 
