eeaf00b394e682381eb6b8f9fbe6417618f2f614
josephtbenton <josephtbenton@gmail.com>
2015-09-17 13:07:53 -0500

    Fixed AlphaBeta search

--- src/chess/ai/AlphaBeta.java
+++ src/chess/ai/AlphaBeta.java
@@ -7,7 +7,7 @@ public class AlphaBeta extends Searcher {
 	@Override
 	public MoveScore findBestMove(Chessboard board, BoardEval eval, int depth) {
 		setup(board, eval, depth);
-		MoveScore result = evalMoves(board, eval, depth,-1,1);
+		MoveScore result = evalMoves(board, eval, depth,-eval.maxValue(),eval.maxValue());
 		System.out.println("Result "+result.getScore() + result.getMove());
 		tearDown();
 		return result;
