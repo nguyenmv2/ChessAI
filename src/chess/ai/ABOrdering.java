cc0381a743dad09120b7abd8bbb92754e5ef4194
My Nguyen <nguyenmv2@hendrix.edu>
2015-09-22 12:33:07 -0500

    Add Ordering Search + Split Quiescene and AlphaBeta

--- src/chess/ai/ABOrdering.java
+++ src/chess/ai/ABOrdering.java
@@ -2,13 +2,17 @@ package chess.ai;
 
 import chess.core.Chessboard;
 import chess.core.Move;
-import chess.gui.Chess;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
-public class AlphaBeta extends Searcher {
-	@Override
+/**
+ * Created by Hoa Dam on 9/22/2015.
+ */
+public class ABOrdering extends Searcher{
+    @Override
 	public MoveScore findBestMove(Chessboard board, BoardEval eval, int depth) {
 		setup(board, eval, depth);
 		MoveScore result = evalMoves(board, eval, depth,-eval.maxValue(),eval.maxValue());
@@ -18,7 +22,16 @@ public class AlphaBeta extends Searcher {
 
 	MoveScore evalMoves(Chessboard board, BoardEval eval, int depth, int alpha, int beta) {
 		MoveScore best = null;
-		for (Move m: board.getLegalMoves()) {
+		List<Move> moveList = board.getLegalMoves();
+		PriorityQueue<MoveScore> moveOrder = new PriorityQueue<>(moveList.size(), new MoveScoreComparator());
+		for (Move m : board.getLegalMoves()){
+			Chessboard next= generate(board, m);
+			MoveScore score = new MoveScore(evaluate(next, eval),m);
+			moveOrder.add(score);
+		}
+
+		while (!moveOrder.isEmpty()){
+			Move m = moveOrder.poll().getMove();
 			Chessboard next = generate(board, m);
 			MoveScore result = new MoveScore(-evalBoard(next, eval, depth - 1, -beta, -alpha), m);
 			if(alpha < result.getScore()) {
@@ -32,15 +45,24 @@ public class AlphaBeta extends Searcher {
 		}
 
 		return best;
+		}
 
-	}	
-	
+
 	int evalBoard(Chessboard board, BoardEval eval, int depth, int alpha, int beta) {
 		if (depth == 0) {
 			return evaluate(board, eval);
 		} else {
 
 			return evalMoves(board, eval, depth,alpha,beta).getScore();
+		}
+	}
+
+	class MoveScoreComparator implements Comparator<MoveScore>{
+		@Override
+		public int compare(MoveScore x, MoveScore y) {
+			if(x.getScore() < y.getScore()) return -1;
+			if(x.getScore() > y.getScore()) return 1;
+			return 0;
 		}
 	}
 
--- src/chess/ai/ABQuiescene.java
+++ src/chess/ai/ABQuiescene.java
@@ -2,12 +2,11 @@ package chess.ai;
 
 import chess.core.Chessboard;
 import chess.core.Move;
-import chess.gui.Chess;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class AlphaBeta extends Searcher {
+public class ABQuiescene extends Searcher {
 	@Override
 	public MoveScore findBestMove(Chessboard board, BoardEval eval, int depth) {
 		setup(board, eval, depth);
@@ -36,13 +35,27 @@ public class AlphaBeta extends Searcher {
 	}	
 	
 	int evalBoard(Chessboard board, BoardEval eval, int depth, int alpha, int beta) {
-		if (depth == 0) {
-			return evaluate(board, eval);
-		} else {
-
+		if (depth == 0) return quiescene(board, eval, alpha, beta);
+		else {
 			return evalMoves(board, eval, depth,alpha,beta).getScore();
 		}
 	}
-
+	int quiescene(Chessboard board, BoardEval eval, int alpha, int beta){
+		int standingVal = evaluate(board, eval);
+		if (standingVal >= beta) return beta;
+		if (standingVal > alpha) alpha = standingVal;
+		List<Move> capturable = new ArrayList<>();
+		for (Move m: board.getLegalMoves()){
+			if(m.getCapture() != null) capturable.add(m);
+		}
+		if (capturable.size() == 0 ) return alpha;
+		for (Move mv : capturable){
+			Chessboard next = generate(board, mv);
+			int score = quiescene(next, eval, -beta, -alpha);
+			if (score >= beta) return beta;
+			if (score > alpha) alpha = score;
+		}
+		return alpha;
+	}
 
 }
