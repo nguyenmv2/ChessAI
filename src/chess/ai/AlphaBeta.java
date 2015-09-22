e47d0bd4bfa0c20c136e14615145b800d91955a1
My Nguyen <nguyenmv2@hendrix.edu>
2015-09-21 20:48:23 -0500

    Add quiescene search

--- src/chess/ai/AlphaBeta.java
+++ src/chess/ai/AlphaBeta.java
@@ -2,6 +2,10 @@ package chess.ai;
 
 import chess.core.Chessboard;
 import chess.core.Move;
+import chess.gui.Chess;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AlphaBeta extends Searcher {
 	@Override
@@ -33,11 +37,27 @@ public class AlphaBeta extends Searcher {
 	
 	int evalBoard(Chessboard board, BoardEval eval, int depth,int alpha, int beta) {
 		if (depth == 0) {
-			return evaluate(board, eval);
+//			return evaluate(board, eval);
+			return quiescene(board,eval,alpha,beta);
 		} else {
 			return evalMoves(board, eval, depth,alpha,beta).getScore();
 		}
 	}
-
+	int quiescene(Chessboard board, BoardEval eval, int alpha, int beta){
+		int standingVal =evaluate(board, eval);
+		if (standingVal >= beta) return beta;
+		if (standingVal > alpha) alpha = standingVal;
+		List<Move> capturable = new ArrayList<>();
+		for (Move m: board.getLegalMoves()){
+			if(m.getCapture() != null) capturable.add(m);
+		}
+		for (Move mv : capturable){
+			Chessboard next = generate(board, mv);
+			int score = quiescene(board, eval, -beta, -alpha);
+			if (score >= beta) return beta;
+			if (score > alpha) alpha = score;
+		}
+		return alpha;
+	}
 
 }
